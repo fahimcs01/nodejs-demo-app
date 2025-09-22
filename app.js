6638a8a19e3df81acd0fa2decdb17bcbd5dd066a
Mohammad Fahim Khan <141926442+Fahimashu@users.noreply.github.com>
2025-09-22 13:44:52 +0530

    Merge remote changes

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 4000; // or 4000 if 3000 is used
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js CI/CD pipeline!");
+  res.send("Hello from Node.js!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
